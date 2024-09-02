b0040fc47c58d24d026f93333e91a47abcb8952a
jeong <tuna1402@PC-202406281447>
2024-09-02 14:46:11 +0900

    main.py -> def, readme -> add contents

--- rag/main.py
+++ rag/main.py
@@ -5,27 +5,33 @@ from langchain.chains import create_history_aware_retriever
 from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 import time
 
-llm = ChatOpenAI(model="gpt-4o-mini", temperature=1, max_tokens=300)
-retriever = pinecone_retriever
-chat_prompt = ChatPromptTemplate.from_messages([
-    MessagesPlaceholder(variable_name='chat_history'),
-    ("user", "{input}"),
-    ("user", "Given the above conversation, \
-     generate a search query to look up \
-     in order to get information relevant to the conversation. \
-     You are an assistant for question-answering tasks about the interior. \
-     Use the following pieces of retrieved context to answer the question. \
-     If you don't know the answer, just say that you don't know.")
-])
+def get_rag_response(question: str) -> str:
+    llm = ChatOpenAI(model="gpt-4o-mini", temperature=1, max_tokens=300)
+    retriever = pinecone_retriever
+    chat_prompt = ChatPromptTemplate.from_messages([
+        MessagesPlaceholder(variable_name='chat_history'),
+        ("user", "{input}"),
+        ("user", "Given the above conversation, generate a search query to look up in order to \
+         get information relevant to the conversation. You are an assistant for question-answering tasks about the interior. \
+         Use the following pieces of retrieved context to answer the question. \
+         If you don't know the answer, just say that you don't know.")
+    ])
 
-retriever_chain = create_history_aware_retriever(llm, retriever, chat_prompt)
+    retriever_chain = create_history_aware_retriever(llm, retriever, chat_prompt)
 
-start_time = time.time()
+    start_time = time.time()
 
-question1 = "가게 인테리어 할 때 필요한 정보는 무엇이 있습니까?"
+    # 질문을 받아 응답 생성
+    res = rag_response(question, retriever_chain)
+    
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"Execution Time: {execution_time} seconds")
 
-res = rag_response(question1, retriever_chain)
-end_time = time.time()
-print(res)
-execution_time = end_time - start_time
-print(execution_time)
+    return res
+
+if __name__ == "__main__":
+    
+    question1 = "가게 인테리어 할 때 필요한 정보는 무엇이 있습니까?"
+    response = get_rag_response(question1)
+    print(response)
